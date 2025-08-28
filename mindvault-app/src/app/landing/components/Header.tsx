48194bda2c8e8f0464cd0766add407c40da5a5eb
Miche1-Pierre <pierre.michel.work@gmail.com>
2025-08-28 21:48:57 +0200

    refactor: update Header components to use new Button component

--- mindvault-app/src/app/landing/components/Header.tsx
+++ mindvault-app/src/app/landing/components/Header.tsx
@@ -1,4 +1,4 @@
-import { NavLink } from "@/components/atoms/NavLink";
+import Button from "@/components/atoms/Button";
 import Image from "next/image";
 import icon from "@/app/favicon.png";
 
@@ -7,7 +7,7 @@ export default function Header() {
     <header className="fixed top-2 z-30 w-full md:top-6">
       <div className="mx-auto max-w-6xl px-4 sm:px-6">
         <div className="relative flex h-14 items-center justify-between gap-3 rounded-2xl backdrop-filter backdrop-blur-[20px] px-3 shadow-lg shadow-black/[0.03] before:pointer-events-none before:absolute before:inset-0 before:rounded-[inherit] before:border before:border-[#cbd5e1] before:[background:linear-gradient(var(--color-gray-100),var(--color-gray-200))_border-box] before:[mask-composite:exclude_!important] before:[mask:linear-gradient(white_0_0)_padding-box,_linear-gradient(white_0_0)]">
-          {/* Site branding */}
+          {/* Logo */}
           <div className="flex flex-1 items-center">
             <Image
               src={icon}
@@ -18,19 +18,24 @@ export default function Header() {
             />
           </div>
 
-          {/* Navigation links */}
-          <ul className="flex flex-1 items-center justify-end gap-3">
-            <NavLink href="/login-register?mode=login" variant="header">
+          {/* Navigation */}
+          <div className="flex flex-1 items-center justify-end gap-3">
+            <Button
+              href="/login-register?mode=login"
+              variant="outlined"
+              size="sm"
+            >
               Login
-            </NavLink>
-            <NavLink
+            </Button>
+            <Button
               href="/login-register?mode=register"
-              variant="header"
-              className="bg-gradient-to-tr from-[#D56434] to-[#6D66E7] text-white"
+              variant="gradient"
+              size="sm"
+              rounded
             >
               Register
-            </NavLink>
-          </ul>
+            </Button>
+          </div>
         </div>
       </div>
     </header>
