4035d75ff30eb0b8b5f3f53138427e128b651bdb
Miche1-Pierre <pierre.michel.work@gmail.com>
2025-08-28 21:48:35 +0200

    refactor: cn function

--- mindvault-app/src/utils/cn.tsx
+++ mindvault-app/src/utils/cn.tsx
@@ -1,15 +1,3 @@
-import { ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-/**
- * Fusionne proprement des classes CSS avec support de Tailwind
- * - clsx : permet d’ajouter des classes conditionnellement
- * - tailwind-merge : supprime les conflits de classes Tailwind
- *
- * Exemple :
- *   cn("px-2", isActive && "bg-red-500", "px-4")
- *   => "bg-red-500 px-4"
- */
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+export function cn(...classes: (string | undefined | null | false)[]) {
+  return classes.filter(Boolean).join(" ");
 }
