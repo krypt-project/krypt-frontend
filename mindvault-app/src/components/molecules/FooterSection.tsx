7085018c280f2beb69bb3ec9eead586c3623a9e4
Miche1-Pierre <pierre.michel.work@gmail.com>
2025-08-29 16:33:58 +0200

    refactor: update footer section

--- mindvault-app/src/components/molecules/FooterSection.tsx
+++ mindvault-app/src/components/molecules/FooterSection.tsx
@@ -10,7 +10,7 @@ export function FooterSection({
   return (
     <div className="space-y-2 sm:col-span-6 md:col-span-3 lg:col-span-2">
       <h3 className="text-black font-medium">{title}</h3>
-      <ul className="space-y-2">
+      <ul className="space-y-0">
         {links.map((label) => (
           <li key={label}>
             <Button href="#" variant="link">
