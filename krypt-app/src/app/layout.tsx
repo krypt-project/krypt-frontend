74662954a55f969ee3d6bd86073a1aed539f06d7
Miche1-Pierre <pierre.michel.work@gmail.com>
2025-09-07 15:55:57 +0200

    refactor: rebranding

---
+++ krypt-app/src/app/layout.tsx
@@ -0,0 +1,48 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "MindVault",
+  icons: "/favicon.png",
+  description: "Your intelligent knowledge base",
+  authors: [
+    {
+      name: "Pierre",
+      url: "https://github.com/Miche1-Pierre",
+    },
+  ],
+  keywords: [
+    "knowledge base",
+    "AI",
+    "intelligent",
+    "mind vault",
+    "personal knowledge management",
+  ],
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ krypt-app/src/app/page.tsx
@@ -0,0 +1,19 @@
+import Header from "@/components/landing-page/Header";
+import Hero from "@/components/landing-page/Hero";
+import Features from "@/components/landing-page/Features";
+import Testimonials from "@/components/landing-page/Testimonials";
+import CTA from "@/components/landing-page/CTA";
+import Footer from "@/components/landing-page/Footer";
+
+export default function Home() {
+  return (
+    <div className="min-h-screen flex flex-col justify-between overflow-hidden">
+      <Header />
+      <Hero />
+      <Features />
+      <Testimonials />
+      <CTA />
+      <Footer />
+    </div>
+  );
+}
