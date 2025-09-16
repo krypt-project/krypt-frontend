fd0399cd82c06931a62a4e4f7b80b16ac614ad15
Miche1-Pierre <pierre.michel.work@gmail.com>
2025-09-16 22:55:12 +0200

    feat: update ActivityBar to set different Popup and display user info

--- krypt-app/src/app/dashboard/components/ActivityBar.tsx
+++ krypt-app/src/app/dashboard/components/ActivityBar.tsx
@@ -1,10 +1,11 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Image from "next/image";
 import { Home, FileText, Settings, LogOut } from "lucide-react";
 
 import Button from "@/components/atoms/Button";
+import Popup from "@/components/atoms/Popup";
 
 type Activity = "home" | "notes" | "settings";
 
@@ -15,6 +16,12 @@ interface ActivityBarProps {
 
 export default function ActivityBar({ active, onSelect }: ActivityBarProps) {
   const [loading, setLoading] = useState(false);
+  const [user, setUser] = useState<{
+    firstName: string;
+    lastName: string;
+    email: string;
+    avatarUrl?: string;
+  } | null>(null);
   const logout = async () => {
     setLoading(true);
 
@@ -51,6 +58,35 @@ export default function ActivityBar({ active, onSelect }: ActivityBarProps) {
     }
   };
 
+  useEffect(() => {
+    const fetchUserData = async () => {
+      const token = localStorage.getItem("token");
+      if (!token) return;
+
+      const userInfoUrl = process.env.NEXT_PUBLIC_USERINFO_URL;
+      if (!userInfoUrl) {
+        console.error("NEXT_PUBLIC_USERINFO_URL is not defined");
+        return;
+      }
+
+      const res = await fetch(userInfoUrl, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      if (!res.ok) {
+        console.error("Failed to fetch user data");
+        return;
+      }
+
+      const data = await res.json();
+      setUser(data);
+    };
+
+    fetchUserData();
+  }, []);
+
   return (
     <div className="w-16 bg-[var(--background)] text-[var(--text-dark)] flex flex-col items-center py-2 space-y-4 border-r border-[var(--border)]">
       <div className="flex items-center justify-center h-15 border-b border-[var(--border)] w-full">
@@ -62,52 +98,89 @@ export default function ActivityBar({ active, onSelect }: ActivityBarProps) {
           className="mb-2"
         />
       </div>
-      <Button
-        onClick={() => onSelect("home")}
-        variant="sidebar"
-        className={`p-2 ${
-          active === "home" || active === null ? "bg-[var(--secondary)] text-[var(--primary)]" : ""
-        }`}
-        title="Home"
-      >
-        <Home size={20} />
-      </Button>
 
-      <Button
-        onClick={() => onSelect("notes")}
-        variant="sidebar"
-        className={`p-2 ${
-          active === "notes"
-            ? "bg-[var(--secondary)] text-[var(--primary)]"
-            : ""
-        }`}
-        title="Notes"
-      >
-        <FileText size={20} />
-      </Button>
-
-      <div className="mt-auto flex flex-col items-center space-y-4 mb-7">
+      <Popup content={<span>Home</span>} position="right">
         <Button
-          onClick={() => onSelect("settings")}
+          onClick={() => onSelect("home")}
           variant="sidebar"
           className={`p-2 ${
-            active === "settings"
+            active === "home" || active === null
               ? "bg-[var(--secondary)] text-[var(--primary)]"
               : ""
           }`}
-          title="Settings"
+          title="Home"
         >
-          <Settings size={20} />
+          <Home size={20} />
         </Button>
+      </Popup>
+
+      <Popup content={<span>Notes</span>} position="right">
         <Button
-          onClick={logout}
+          onClick={() => onSelect("notes")}
           variant="sidebar"
-          className={`p-2 text-[var(--error)] bg-[var(--error)]/20`}
-          title="Logout"
+          className={`p-2 ${
+            active === "notes"
+              ? "bg-[var(--secondary)] text-[var(--primary)]"
+              : ""
+          }`}
+          title="Notes"
         >
-          <LogOut size={20} />
+          <FileText size={20} />
         </Button>
+      </Popup>
+
+      <div className="mt-auto flex flex-col items-center space-y-4 mb-7">
+        <Popup content={<span>Settings</span>} position="right">
+          <Button
+            onClick={() => onSelect("settings")}
+            variant="sidebar"
+            className={`p-2 ${
+              active === "settings"
+                ? "bg-[var(--secondary)] text-[var(--primary)]"
+                : ""
+            }`}
+            title="Settings"
+          >
+            <Settings size={20} />
+          </Button>
+        </Popup>
+
+        <Popup content={<span>Logout</span>} position="right">
+          <Button
+            onClick={logout}
+            variant="sidebar"
+            className={`p-2 text-[var(--error)] bg-[var(--error)]/20`}
+            title="Logout"
+          >
+            <LogOut size={20} />
+          </Button>
+        </Popup>
       </div>
+
+      {/* User avatar with popup */}
+      {user && (
+        <div className="relative mb-4">
+          <Popup
+            content={
+              <div>
+                <p className="font-semibold">
+                  {user.firstName} {user.lastName}
+                </p>
+                <p className="text-sm text-gray-500">{user.email}</p>
+              </div>
+            }
+            position="right"
+          >
+            <Image
+              src={user.avatarUrl || "/profil.png"}
+              className="w-13 h-13 rounded-full cursor-pointer"
+              alt="User Avatar"
+              width={50}
+              height={50}
+            />
+          </Popup>
+        </div>
+      )}
     </div>
   );
 }
