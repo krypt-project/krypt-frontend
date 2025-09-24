c79144cccdc5d3f3e1e5d733f88a187b93888f82
Miche1-Pierre <pierre.michel.work@gmail.com>
2025-09-24 17:01:49 +0200

    feat: create MarketPlace page

---
+++ krypt-app/src/app/dashboard/components/marketplace/MarketPlace.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { useState } from "react";
+import { ALL_MODULES, Module } from "@/config/constants";
+import { FeatureCard } from "@/components/molecules/FeatureCard";
+import MarketplaceNavbar from "./components/MarketplaceNavbar";
+
+export default function Marketplace() {
+  const [filter, setFilter] = useState<Module["category"] | "All">("All");
+
+  const filteredModules =
+    filter === "All"
+      ? ALL_MODULES
+      : ALL_MODULES.filter((m) => m.category === filter);
+
+  return (
+    <>
+      <MarketplaceNavbar filter={filter} setFilter={setFilter} />
+
+      <div className="flex flex-1 justify-center w-full bg-[var(--secondary)]">
+        <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3 mt-25 mb-10">
+          {filteredModules.map((m, idx) => (
+            <FeatureCard key={idx} {...m} />
+          ))}
+        </div>
+      </div>
+    </>
+  );
+}
